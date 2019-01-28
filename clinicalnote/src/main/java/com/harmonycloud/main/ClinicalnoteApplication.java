fd8fe0f9c42b2ccdda3f850a8ddc1de861611241
余倩 <qianyu@harmonycloud.cn>
2019-01-28 16:28:23 +0800

    init

---
+++ clinicalnote/src/main/java/com/harmonycloud/main/ClinicalnoteApplication.java
@@ -0,0 +1,14 @@
+package com.harmonycloud.clinicalnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicalnoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClinicalnoteApplication.class, args);
+    }
+
+}
+
---
+++ diagnosis/src/main/java/com/harmonycloud/main/DiagnosisApplication.java
@@ -0,0 +1,14 @@
+package com.harmonycloud.diagnosis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiagnosisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiagnosisApplication.class, args);
+    }
+
+}
+
